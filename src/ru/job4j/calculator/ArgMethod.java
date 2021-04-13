b7d8024d5d19039476d7be3975d94aef2fb6a985
shabad1n <shabadin@icloud.com>
2021-04-13 14:50:24 +0300

    3.1 Фиксация изменений на github [#243567]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -2,12 +2,12 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + "; your age " + age + " years old");
     }
 
     public static void main(String[] args) {
-        String name = "Petr Arsentev";
-        int age = 33;
+        String name = "Kirill Gavrilov";
+        int age = 25;
 
         ArgMethod.hello(name, age);
 
