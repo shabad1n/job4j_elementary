08698bd6681d81f08c94379924ccccc62dd79335
shabad1n <shabadin@icloud.com>
2021-04-14 16:07:30 +0300

    3.1 Загрузка файла на github [#243573]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int age = 2021;
+        System.out.println(idea + "But I am a newbie. " + age);
+    }
+}
