b30e1016122f0c1da364b5d8dc5f6e350c0e78ba
shabad1n <shabadin@icloud.com>
2021-04-15 08:19:17 +0300

    3.1 Фиксация изменений на github [#243574]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,7 +3,10 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
-        int age = 2021;
-        System.out.println(idea + "But I am a newbie. " + age);
+        System.out.println(idea);
+        idea = idea + "But I am a newbie. ";
+        int year = 2021;
+        idea = idea + year;
+        System.out.println(idea);
     }
 }
