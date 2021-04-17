355d202212654a1133024dfcf0794c84f677b5b5
shabad1n <Karabanovo0308>
2021-04-17 12:18:54 +0300

    3.1 Фиксация изменения на githab[#243582]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -9,11 +9,11 @@ public class Converter {
         return d/60;
     }
 
-    public static void main(String[] args) {
+    public static <String> void main(String[] args) {
         int in = 140;
         int expected = 2;
         int out = Converter.rubleToDollar(in);
-        boolean passed1 = expected == out;
+        var passed1 = expected == out;
         int out1 = Converter.rubleToEuro(in);
         boolean passed2 = expected == out1;
         System.out.println("60 rubles are 1 dollar : " + passed1);
