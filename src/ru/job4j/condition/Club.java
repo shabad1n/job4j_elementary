7e99585fdcd924854ad65b8e5c4e9a5d8f907a6e
shabad1n <Karabanovo0308>
2021-04-18 14:52:02 +0300

    3.1 Загрузка файла на githab[#243596]

--- src/ru/job4j/condition/Club.java
+++ src/ru/job4j/condition/Club.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Club {
-    public static void permission(boolean hasMoney, boolean beFriend {
+    public static void permission(boolean hasMoney, boolean beFriend) {
         boolean rls = (hasMoney || beFriend);
         if(rls) {
             System.out.println("I can go to the club.");
