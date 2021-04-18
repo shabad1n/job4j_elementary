654db4e7305db6a3188aa956c8b03fee00231e52
shabad1n <Karabanovo0308>
2021-04-18 15:11:12 +0300

    3.1 Загрузка файла на githab[#243599]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -4,16 +4,16 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот".equals(question)) {
-            rsl = "Привет умник";
-        } else if ("Пока".equals(question)) {
-            rsl = "До скорой встречи";
+            rsl = "Привет, умник";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
         }
         return rsl;
     }
     public static void main(String[]args) {
         String rsl = DummyBot.answer("Привет, Бот");
         System.out.println(rsl);
-        rsl = DummyBot.answer("Пока");
+        rsl = DummyBot.answer("Пока.");
         System.out.println(rsl);
     }
 }
