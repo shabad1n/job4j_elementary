8db0c0a34e6d4e607c9e8f5f3db6497d0ba0818f
shabad1n <Karabanovo0308>
2021-04-18 14:09:37 +0300

    3.1 Фиксация изменений на githab[#243594]

--- src/ru/job4j/condition/Club.java
+++ src/ru/job4j/condition/Club.java
@@ -1,11 +1,12 @@
 package ru.job4j.condition;
 
 public class Club {
-    public static void permission(boolean hasMoney, boolean beFriend){
+    public static void permission(boolean hasMoney, boolean beFriend {
         boolean rls = (hasMoney || beFriend);
         if(rls) {
             System.out.println("I can go to the club.");
-        } else {
+        }
+        else {
             System.out.println("I can't.");
         }
     }
