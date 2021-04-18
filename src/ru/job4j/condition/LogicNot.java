783c4bcf57d52228dcdfec083e500fbc11ad2d8c
shabad1n <Karabanovo0308>
2021-04-18 14:01:01 +0300

    3.1 Фиксация изменений на githab[#243593]

--- src/ru/job4j/condition/LogicNot.java
+++ src/ru/job4j/condition/LogicNot.java
@@ -4,21 +4,27 @@ public class LogicNot {
     public static boolean isEven(int num) {
         return num % 2 == 0;
     }
+
     public static boolean isPositive(int num) {
         return num > 0;
     }
+
     public static boolean notEven(int num) {
         return !isEven(num);
     }
+
     public static boolean notPositive(int num) {
         return !isPositive(num);
     }
+
     public static boolean notEvenAndPositive(int num) {
-        return !(isEven(num)&&isPositive(num));
+        return (!(isEven(num))&&isPositive(num));
     }
+
     public static boolean evenOrNotPositive(int num) {
-        return !(isEven(num)&&isPositive(num));
+        return ((isEven(num))&&!(isPositive(num)));
     }
+
     public static void main(String[]args){
        boolean rls =  evenOrNotPositive(4);
         System.out.println(rls);
