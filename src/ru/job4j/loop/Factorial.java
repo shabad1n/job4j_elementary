0f2b3b84526cd7f63c96307026d6a7711012bb9b
shabad1n <shabadin@icloud.com>
2021-04-21 10:04:11 +0300

    5.2. Создать программу, вычисляющую факториал. [#517343]

---
+++ src/ru/job4j/loop/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
+    public static int calc(int n) {
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+        return sum;
+    }
+
+    public static void main(String[]args) {
+        System.out.println(calc(1));
+    }
+}
