dbb72c2e7b6153c1def1886d7b3671416ead1477
shabad1n <shabadin@icloud.com>
2021-04-21 12:57:07 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [# 517342]

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -2,21 +2,18 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = 0;
+        int result = third;
         if (first >= second && first >= third) {
            result = first;
         }
         if (second >= first && second >= third) {
             result = second;
         }
-        if (third >= first && third >= second) {
-            result = third;
-        }
         return result;
     }
 
     public static void main(String[]args) {
-        int rsl = ThreeMax.max(10, 5, 1);
+        int rsl = ThreeMax.max(10, 2, 1);
         System.out.println(rsl);
     }
 }
