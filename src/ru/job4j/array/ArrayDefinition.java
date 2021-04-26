94fe31dcdcd71a3736976efc846becd10a7418ce
shabad1n <shabadin@icloud.com>
2021-04-26 08:46:58 +0300

    6.0.3. Массивы и цикл for. [#517258]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -11,7 +11,7 @@ public class ArrayDefinition {
         String[] names = new String[4];
         names[0] = "Hey";
         names[1] = "I";
-        names[2] = "Kirill";
+        names[2] = "Konor";
         names[3] = "Gavrilov";
         for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
